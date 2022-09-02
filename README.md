83ab530fe9cebde7cdf2aaa4081bf1d2d4117e71
mattcroat <mattcroat.dev@gmail.com>
2022-09-02 17:11:21 +0200

    feat: Add example

---
+++ README.md
@@ -0,0 +1,21 @@
+# Svelte State Management
+
+✍️ https://joyofcode.xyz/using-future-css-in-svelte
+
+Learn how to use future CSS in Svelte today with PostCSS.
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/joysofcode/svelte-future-css)
+
+## 💿️ Development
+
+You can use any package manager.
+
+```bash
+pnpm i && pnpm run dev
+```
+
+## 🛠️ Build
+
+```bash
+pnpm run build && pnpm run preview
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<svelte:head>
+	<title>Future CSS</title>
+</svelte:head>
+
+<h1>Use Future CSS Today 😎</h1>
+
+<style lang="postcss">
+	:global(body) {
+		height: 100vh;
+		display: grid;
+		place-content: center;
+		margin: 0;
+		color: hsl(220 20% 98%);
+		background-color: hsl(220 20% 14%);
+		font-family: sans-serif;
+	}
+
+	/* Nesting */
+
+	@layer base {
+		h1 {
+			color: white;
+			transition: transform 10s ease;
+
+			&:hover {
+				transform: rotate(2turn);
+			}
+		}
+	}
+
+	/* Layers */
+
+	@layer typography {
+		h1 {
+			font-size: 3rem;
+			color: aquamarine;
+		}
+	}
+
+	/* Media queries */
+
+	@custom-media --narrow (max-width: 400px);
+
+	@media (--narrow) {
+		h1 {
+			color: tomato;
+		}
+	}
+
+	@media (width < 200px) {
+		h1 {
+			color: palegreen;
+		}
+	}
+</style>
